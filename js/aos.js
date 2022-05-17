c0ed301a202df78884ff86d9b5ce6dd993e1a57c
PhoenixllGamer <jshsh6@gmail.com>
2022-05-17 19:43:42 +0300

    update

--- js/aos.js
+++ js/aos.js
@@ -1,18 +1,41 @@
-AOS.init({
-    disable: false,
-    startEvent: 'DOMContentLoaded',
-    initClassName: 'aos-init',
-    animatedClassName: 'aos-animate',
-    useClassNames: false,
-    disableMutationObserver: false,
-    debounceDelay: 50,
-    throttleDelay: 99,
+$(document).ready(function() {
+    if (window.innerHeight >= 1400) {
+        AOS.init({
+            disable: false,
+            startEvent: 'DOMContentLoaded',
+            initClassName: 'aos-init',
+            animatedClassName: 'aos-animate',
+            useClassNames: false,
+            disableMutationObserver: false,
+            debounceDelay: 50,
+            throttleDelay: 99,
 
-    offset: 550,
-    delay: 0,
-    duration: 400,
-    easing: 'ease',
-    once: false,
-    mirror: false,
-    anchorPlacement: 'top-bottom',
+            offset: 300,
+            delay: 0,
+            duration: 400,
+            easing: 'ease',
+            once: false,
+            mirror: false,
+            anchorPlacement: 'top-bottom',
+        });
+    } else {
+        AOS.init({
+            disable: false,
+            startEvent: 'DOMContentLoaded',
+            initClassName: 'aos-init',
+            animatedClassName: 'aos-animate',
+            useClassNames: false,
+            disableMutationObserver: false,
+            debounceDelay: 50,
+            throttleDelay: 99,
+
+            offset: 120,
+            delay: 0,
+            duration: 400,
+            easing: 'ease',
+            once: false,
+            mirror: false,
+            anchorPlacement: 'top-bottom',
+        });
+    }
 });
--- js/autosize.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-    document.getElementById('scr').style.minHeight = window.innerHeight + 10 + "px";
-});
