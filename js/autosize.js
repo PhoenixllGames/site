18b229f62384e4ef544e6eac7f31abd2e664a36d
PhoenixllGamer <jshsh6@gmail.com>
2022-05-17 06:40:21 +0300

    update

---
+++ js/autosize.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    document.getElementById('scr').style.minHeight = window.innerHeight + 10 + "px";
+});
---
+++ js/load.js
@@ -0,0 +1,5 @@
+$(function() {
+    $("#header").load("header.html");
+    $("#headers").load("headers.html");
+    $("#footer").load("footer.html");
+});
